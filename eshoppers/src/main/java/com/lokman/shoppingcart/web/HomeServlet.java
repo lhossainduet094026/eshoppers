590d9adc5e1585ace399e39a77d487718d8def96
user <user@192.168.123.9>
2021-04-19 23:29:58 +0600

    --Logback logger added.

--- eshoppers/src/main/java/com/lokman/shoppingcart/web/HomeServlet.java
+++ eshoppers/src/main/java/com/lokman/shoppingcart/web/HomeServlet.java
@@ -9,20 +9,28 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.LoggerFactory;
+
 import com.lokman.shoppingcart.dto.ProductDTO;
 import com.lokman.shoppingcart.repository.DummyProductRepositoryImpl;
 import com.lokman.shoppingcart.service.ProductService;
 import com.lokman.shoppingcart.service.ProductServiceImpl;
 
+import ch.qos.logback.classic.Logger;
+
+
+
+
 @WebServlet("/home")
 public class HomeServlet extends HttpServlet {
 	private ProductService productService =new ProductServiceImpl(new DummyProductRepositoryImpl());
-
+    private static final Logger LOGGER=(Logger) LoggerFactory.getLogger(HomeServlet.class);
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+		LOGGER.info("do get method calls");
 		List<ProductDTO> allProducts = productService.findAllProductSortedByName();
 		req.setAttribute("products", allProducts);
+		
 		req.getRequestDispatcher("/WEB-INF/home.jsp").forward(req, resp);
 	}
 }
