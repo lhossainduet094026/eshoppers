eb3840bb95312f8069e994c83eca1baa682e4349
user <user@192.168.123.7>
2021-04-24 22:52:39 +0600

    --UserDTO class added. --doPost() implementd for signup . --copyParametersTo() method added in SignupServlet.

--- eshoppers/src/main/java/com/lokman/shoppingcart/web/SignupServlet.java
+++ eshoppers/src/main/java/com/lokman/shoppingcart/web/SignupServlet.java
@@ -9,6 +9,8 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.LoggerFactory;
 
+import com.lokman.shoppingcart.dto.UserDTO;
+
 import ch.qos.logback.classic.Logger;
 
 /**
@@ -16,18 +18,50 @@ import ch.qos.logback.classic.Logger;
  */
 @WebServlet("/signup")
 public class SignupServlet extends HttpServlet {
-private static final Logger LOGGER=(Logger) LoggerFactory.getLogger(SignupServlet.class);
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	//private UserService userService;
+	private static final Logger LOGGER = (Logger) LoggerFactory.getLogger(SignupServlet.class);
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		LOGGER.info("Serving signup page");
 		request.getRequestDispatcher("/WEB-INF/signup.jsp").forward(request, response);
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		UserDTO userDTO = copyParametersTo(request);
+
+		if (isValid(userDTO)) {
+			LOGGER.info("user is valid ,creating a new user with:{}", userDTO);
+			//userService.saveUser(userDTO);
+			response.sendRedirect("/home");
+		} else {
+			LOGGER.info("user sent invalid data:{}", userDTO);
+			request.getRequestDispatcher("/WEB-INF/signup.jsp").forward(request, response);
+		}
+
+	}
+
+	private boolean isValid(UserDTO userDTO) {
+		// will implement it later
+		return false;
+	}
+
+	private UserDTO copyParametersTo(HttpServletRequest request) {
+		var UserDTO = new UserDTO();
+
+		UserDTO.setUsername(request.getParameter("username"));
+		UserDTO.setPassword(request.getParameter("password"));
+		UserDTO.setPasswordConfirmed(request.getParameter("passwordConfirmed"));
+		UserDTO.setEmail(request.getParameter("email"));
+		UserDTO.setFirstName(request.getParameter("firstName"));
+		UserDTO.setLastName(request.getParameter("lastName"));
+		return UserDTO;
 	}
 
 }
