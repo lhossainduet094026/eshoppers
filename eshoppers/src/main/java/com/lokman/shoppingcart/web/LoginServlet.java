d20cf17f3fc1c60d099bf6cf38b996fbe85abc5a
user <user@192.168.123.7>
2021-05-01 23:15:10 +0600

    --bean validator added in gradle file . --login servlet added.

---
+++ eshoppers/src/main/java/com/lokman/shoppingcart/web/LoginServlet.java
@@ -0,0 +1,39 @@
+package com.lokman.shoppingcart.web;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.LoggerFactory;
+
+import ch.qos.logback.classic.Logger;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+
+	private static final Logger LOGGER = (Logger) LoggerFactory.getLogger(LoginServlet.class);
+
+	public LoginServlet() {
+
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		LOGGER.info("Serving login page");
+		request.getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
