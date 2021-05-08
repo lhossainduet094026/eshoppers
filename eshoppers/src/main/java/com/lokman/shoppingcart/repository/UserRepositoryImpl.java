ce44ff4ae5f586e1738c2f598c86fc71d7248044
user <user@192.168.123.9>
2021-05-08 22:52:08 +0600

    --findByName() implemented in UserRepositoryImpl class.

--- eshoppers/src/main/java/com/lokman/shoppingcart/repository/UserRepositoryImpl.java
+++ eshoppers/src/main/java/com/lokman/shoppingcart/repository/UserRepositoryImpl.java
@@ -1,5 +1,6 @@
 package com.lokman.shoppingcart.repository;
 
+import java.util.Iterator;
 import java.util.Set;
 import java.util.concurrent.CopyOnWriteArraySet;
 
@@ -8,15 +9,24 @@ import com.lokman.shoppingcart.domain.User;
 public class UserRepositoryImpl implements UserRepository {
 
 	private static final Set<User> USERS = new CopyOnWriteArraySet<>();
+
 	@Override
 	public void save(User user) {
 		USERS.add(user);
 
 	}
+
 	@Override
 	public User findByName(String username) {
-		// TODO Auto-generated method stub
+		if (username != null) {
+			Iterator<User> it = USERS.iterator();
+			while (it.hasNext()) {
+				User user = it.next();
+				if (user.getFirstName().equals(username)) {
+					return user;
+				}
+			}
+		}
 		return null;
 	}
-
 }
